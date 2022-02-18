aadaeefb9e5555809df76abb601817b362297c2d
Murali-Kalla <kmuralidhar2019@gmail.com>
2022-02-17 20:44:39 -0600

    Calculator App
    Reshwanth Gundeti

---
+++ Gundeti_Calculator/Gundeti_Calculator/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Gundeti_Calculator
+//
+//  Created by Gundeti,Reshwanth on 2/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func buttonAC(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonC(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonChangeSign(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonDiv(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonSeven(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonEight(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonNine(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonMultiply(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonFour(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonFive(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonSix(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonMinus(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonOne(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonTwo(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonThree(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonPlus(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonZero(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonDecimal(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonPercentile(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonEquals(_ sender: UIButton) {
+    }
+
+
+}
+
